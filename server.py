ec2b20ffe1ae98e03e524f6b1aa0f5872931d9da
Artur1221 <artur.saf123124@gmail.com>
2024-11-16 15:23:14 +0000

    Added threading, changed client connection scheme

--- server.py
+++ server.py
@@ -1,87 +1,56 @@
 import socket
-import time
+from threading import Thread
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Server:
+    clients = []
 
-#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('', 8080))
+        self.socket.listen(100)
 
-#s.bind(("", 6700))
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-#s.listen(100)
+        self.socket_t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_t.bind(('', 6700))
+        self.socket_t.listen(100)
 
-#sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_t.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    def listen(self):
+        print('listening')
+        while True:
+            conn, addr = self.socket.accept()
 
-#sc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            Server.clients.append(conn)
+            Thread(target=self.handle_new_client, args=(conn,)).start()
 
-#sc.bind(("", 8080))
+    def handle_new_client(self, conn):
+        print('new client')
+        while True:
+            conn_t, addr_t = self.socket_t.accept()
 
-#sc.listen(100)
+            message = conn_t.recv(2**20)
 
-count = 0
+            login_bytes = bytes([0x23, 0x41, 0x4c, 0x23, 0x31, 0x0d, 0x0a])
 
-login_bytes = bytes([0x23, 0x41, 0x4c, 0x23, 0x31, 0x0d, 0x0a])
+            conn_t.send(login_bytes)
 
-while True:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            message = conn_t.recv(2**20)
 
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.broadcast(message)
+            
+            pkg_amt = len(bytes.fromhex(message.hex(" ")).decode('ascii').split('|')) - 1
+            
+            conn_t.send(bytes([0x23, 0x41, 0x42, 0x23, 0x30 + pkg_amt, 0x0d, 0x0a]))
 
-	s.bind(("", 6700))
+            conn_t.close()
+    
+    def broadcast(self, message):
+        print('broadcasting')
+        for client in self.clients:
+            client.send(message)
 
-	s.listen(100)
-	conn, addr = s.accept()
-	data = conn.recv(2**20)
-
-	login_bytes = bytes([0x23, 0x41, 0x4c, 0x23, 0x31, 0x0d, 0x0a])
-
-	conn.send(login_bytes)
-
-	data = conn.recv(2**20)
-
-	if data:
-		#connc, addrc = sc.accept()
-
-		#datac = connc.recv(2**20)
-
-		print(count)
-
-		print(bytes.fromhex(data.hex(" ")).decode('ascii'))
-		print(data.hex(" "))
-
-		with open('server_output.txt', 'a') as output:
-			output.write(bytes.fromhex(data.hex(" ")).decode('ascii'))
-		pkg_amt = len(bytes.fromhex(data.hex(" ")).decode('ascii').split('|')) - 1
-
-		print(bytes([0x23, 0x41, 0x42, 0x23, 0x37, 0x0d, 0x0a]))
-
-		conn.send(bytes([0x23, 0x41, 0x42, 0x23, 0x30 + pkg_amt, 0x0d, 0x0a]))
-
-		#conn.sendall(data)
-
-		count += 1
-	else:
-		print('No data')
-
-	conn.close()
-	s.close()
-
-	sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	sc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-	sc.bind(("", 8080))
-
-	sc.listen(100)
-
-	connc, addrc = sc.accept()
-
-	#datac = connc.recv(2**20)
-
-	#print(datac)
-
-	connc.send(data)
-
-	connc.close()
-
-
+server = Server()
+server.listen()
 
